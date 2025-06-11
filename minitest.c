b85e2121c36c1f8508f38a28db8fa033e03d23a4
magamagnific <mariafornovikittys@gmail.com>
2025-06-11 11:56:03 +0200

    Un archivo para jugar con lo básico antes de implementarlo al proyecto

---
+++ minitest.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <term.h>
+#include <termios.h>
+#include <stdlib.h>
+
+int main(int ac, char **av, char **env)
+{
+	char *line;
+	int i = 1;
+	
+	while ()
+}
