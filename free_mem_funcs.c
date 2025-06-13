4d58f25b16be5f9f13bcdc6c462bc527c0c509e2
Elena Garcia Arroyo <elenagarciaarroyo@MacBook-Pro-de-Elena.local>
2025-06-13 10:18:29 +0200

    tokenizer_function

---
+++ free_mem_funcs.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_mem_funcs.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elengarc <elengarc@student.42Madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 10:52:26 by elengarc          #+#    #+#             */
+/*   Updated: 2025/06/12 10:52:28 by elengarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	free_token_list(t_token *command)
+{
+	t_token	*tmp;
+
+	while (command)
+	{
+		tmp = command->next;
+		free(command->value);
+		free(command);
+		command = tmp;
+	}
+}
+
+void	free_split(char **split)
+{
+	int	i;
+
+	if (!split)
+		return;
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
--- minishell.h
+++ minishell.h
@@ -19,19 +19,44 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <signal.h>
-# include "libft.h"
+# include "libft/libft.h"
 
-/*  --------ESTRUCTURAS--------*/
+/*  -------- ESTRUCTURAS --------*/
+
+
+typedef struct s_zone {
+	int start;
+	int end;
+	int expandable;
+	struct s_zone *next;
+}   t_zone;
 
 typedef struct s_token {
-    char    *value;
-    bool    quoted;
-    bool    expandable;
-	t_token *next;
-} t_token;
-
+	char *value;
+	t_zone *zones;
+	struct s_token *next;
+}   t_token;
 
 /* --------- PARSEO --------- */
 void	parse_and_execute_prompt(char *prompt);
+int		check_quotes(char *line);
+int		is_metachar(char c);
+int 	is_quote(char c);
+t_zone *create_zone(int start, int end, int expandable);
+void 	add_zone(t_token *token, t_zone *zone);
+char 	*extract_token_value(char *str, int start, int end);
+t_token *new_token(char *str, int start, int end);
+int 	process_quoted_zone(char *str, int *i, t_token *tok);
+int 	find_token_end(char *str, int i);
+void 	append_token(t_token **head, t_token *new_tok);
+t_token *tokenizer(char *cleaned);
+void	relativize_zones(t_token *tokens, char *cleaned);
+
+/* --------- ERRORES ----------*/
+void	error_bad_quoting(void);
+
+/* ---------- LIBERACIÓN DE MEMORIA ---------------*/
+void	free_token_list(t_token *command);
+void	free_split(char **split);
 
 #endif
