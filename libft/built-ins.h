f8ece1fbd3428603a7b593be51a043ac54b507b7
Maria Fornovi Gomez <mfornovi@c1r9s2.42madrid.com>
2025-06-26 14:54:41 +0200

    .

---
+++ libft/built-ins.h
@@ -0,0 +1,8 @@
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
