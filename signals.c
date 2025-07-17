d2cb1884ef6863488aed99f332d3a3219db3d10c
Maria Fornovi Gomez <mfornovi@c1r15s4.42madrid.com>
2025-07-17 10:51:03 +0200

    ctrl+c arreglado

--- signals.c
+++ signals.c
@@ -15,11 +15,14 @@
 void	ft_handle_int(int sig)
 {
 	if (g_signal.ff == 1)
+	{
+		printf("\n");
 		return ;
+	}
 	(void)sig;
 	printf("\n");
-	rl_replace_line("", 1);
 	rl_on_new_line();
+	rl_replace_line("", 1);
 	rl_redisplay();
 	g_signal.ret = 130;
 }
@@ -30,3 +33,4 @@ void	handle_signals(void)
 	signal(SIGINT, ft_handle_int);
 	signal(SIGTSTP, SIG_IGN);
 }
+
